294af8c693743f815c60229dac255343dc257f34
Zach Lamprich <zlamprich.dev@gmail.com>
2024-12-10 22:03:42 -0700

    Add overlay page and new user prompt CSS/JS

--- src/main.js
+++ src/main.js
@@ -1,3 +1,26 @@
 import './style.css'
 
 
+function newUserOverlay() {
+    const contentPane = document.getElementById('contentPane')
+    const newUser = document.createElement('div')
+    const newUserList = document.createElement('div')
+    newUser.setAttribute('id', 'userPrompt')
+    newUserList.setAttribute('id', 'newUserList')
+    newUser.textContent = 'Stay on top of your day with a sleek, ' +
+        'easy-to-use to-do list built for busy professionals and productivity enthusiasts alike. ' +
+        'Whether you\'re tackling work tasks, managing personal goals, or just trying to remember what\'s ' +
+        'for dinner, we\'ve got you covered. Organize, prioritize, and achieve—because your time matters.'
+
+    newUserList.textContent = 'Start Here'
+    contentPane.appendChild(newUser)
+    contentPane.appendChild(newUserList)
+
+    newUserList.addEventListener('click', (e) => {
+        contentPane.innerHTML = ''
+        contentPane.style.backgroundColor = 'transparent'
+    })
+}
+
+newUserOverlay()
+
